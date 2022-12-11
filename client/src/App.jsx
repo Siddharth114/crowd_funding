6ae38fbf3824d7656a2f591ce6b61b235cb6617d
Siddharth <2004sid@gmail.com>
2022-12-11 22:07:58 +0530

    added components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import {CampaignDetails, CreateCampaign, Profile, Home} from './pages';
-import {Sidebar, Navbar} from './components';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { CampaignDetails, CreateCampaign, Profile, Home } from "./pages";
+import { Sidebar, Navbar } from "./components";
 
 const App = () => {
   return (
-    <div className='relative sm:-8 p-4 bg-[#131318] min-h-screen flex flex-row'>
-      <div className='sm:flex hidden mr-10 relative'>
+    <div className="relative sm:-8 p-4 bg-[#131318] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
         <Sidebar />
       </div>
-      
 
-      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
-        <Navbar/>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
       </div>
       <Routes>
         <Route path="" element={<Home />} />
       </Routes>
-
     </div>
   );
-}
+};
 
-export default App
+export default App;
