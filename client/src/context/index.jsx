55281b6d16522836e640080abe64c90a287cc406
Siddharth <2004sid@gmail.com>
2022-12-20 21:47:06 +0530

    fund card

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -25,21 +25,23 @@ export const StateContextProvider = ({ children }) => {
   const publishCampaign = async (form) => {
     try {
       const data = await createCampaign([
-        address,
-        form.title,
-        form.description,
+        address, // owner
+        form.title, // title
+        form.description, // description
         form.target,
-        new Date(form.deadline).getTime(),
-        form.image,
-      ]);
-      console.log("contract call success", data);
+        new Date(form.deadline).getTime(), // deadline,
+        form.image
+      ])
+
+      console.log("contract call success", data)
     } catch (error) {
-      console.log("contract call failure", error);
+      console.log("contract call failure", error)
     }
-  };
+  }
+
 
   const getCampaigns = async () => {
-    const campaigns = await contract.call("getCampaigns");
+    const campaigns = await contract.call('getCampaigns');
 
     const parsedCampaigns = campaigns.map((campaign, i) => ({
       owner: campaign.owner,
--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -18,42 +18,36 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(
-        address _owner,
-        string memory _title,
-        string memory _description,
-        uint256 _target,
-        uint256 _deadline,
-        string memory _image
-    ) public returns (uint256) {
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require (campaign.deadline < block.timestamp, "The deadline should be a date in the future");
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
         campaign.owner = _owner;
         campaign.title = _title;
         campaign.description = _description;
+        campaign.target = _target;
         campaign.deadline = _deadline;
         campaign.amountCollected = 0;
         campaign.image = _image;
 
         numberOfCampaigns++;
 
-        return numberOfCampaigns-1;
-
+        return numberOfCampaigns - 1;
     }
 
     function donateToCampaign(uint256 _id) public payable {
-        uint256 amount=msg.value;
+        uint256 amount = msg.value;
 
         Campaign storage campaign = campaigns[_id];
 
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value:amount}("");
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
 
-        if (sent){
-            campaign.amountCollected=campaign.amountCollected+amount;
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
 
@@ -64,8 +58,9 @@ contract CrowdFunding {
     function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        for (uint i=0; i <numberOfCampaigns;i++){
+        for(uint i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
+
             allCampaigns[i] = item;
         }
 
