a664d597f7d622aceb5cd0d11da94bf5c23feb1e
Siddharth <2004sid@gmail.com>
2022-12-21 21:16:38 +0530

    profile page

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -56,6 +56,13 @@ export const StateContextProvider = ({ children }) => {
     return parsedCampaigns;
   }
 
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner ===address);
+    return filteredCampaigns
+  }
+
   return (
     <StateContext.Provider
       value={{
@@ -63,7 +70,8 @@ export const StateContextProvider = ({ children }) => {
         contract,
         connect,
         createCampaign: publishCampaign,
-        getCampaigns
+        getCampaigns,
+        getUserCampaigns
       }}
     >
       {children}
